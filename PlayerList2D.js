4e8aef8d0d56ff244a6bfa554dca52fa35248544
3dragons <3dragons@cd.com>
2023-08-26 21:43:05 +0800

    undata PlayerList2D && Tools

--- PlayerList2D.js
+++ PlayerList2D.js
@@ -2,7 +2,7 @@
 var scriptName = 'PlayerList2D';
 
 // 定义脚本版本
-var scriptVersion = '2.1.0';
+var scriptVersion = '2.2.0';
 
 // 定义脚本作者
 var scriptAuthor = ['ColdDragon'];
@@ -10,10 +10,13 @@ var scriptAuthor = ['ColdDragon'];
 // 引入 [ Tools.js ] 脚本工具 用于获取工具渲染列表
 script.import('Tools.js');
 
+// 引入实体玩家 Class
+var EntityPlayer = Java.type('net.minecraft.entity.player.EntityPlayer');
+
 // 定义模块的构造函数
 function thePlayerList2D() {
 
-    // 定义 [ setting ] 对象 用于提供选项
+    // 定义 [ setting ] 对象 用于提供选项 Object
     var setting = {
 
         // 布尔值选项 [ string名称 boolean默认值 ]
@@ -22,17 +25,26 @@ function thePlayerList2D() {
         }
     };
 
-    // 定义选项
+    // 定义选项 Object
     var settings = {
 
-        // 是否暴力不显示 NPC 仅靠坐标模糊判断
-        noNPC: setting.boolean('NOArcadeNPC', true),
+        // 是否防止渲染NPC
+        noNPC: setting.boolean('AntiNPC', true),
 
-        // 当 z 大于 700 时不渲染工具
+        // 是否自动渲染工具 当不在游戏时就不渲染工具
         renderTools: setting.boolean('AutoRenderTools', true),
+
+        // 是否渲染工具 关闭此项将不会渲染工具列表
+        toolsList2D: setting.boolean('ToolsList2D', true),
+
+        // 是否防止渲染假人
+        noBot: setting.boolean('AntiBot', true),
+
+        // 显示隐藏的玩家
+        invisible: setting.boolean('invisible', false),
     };
 
-    // 定义模块选项 
+    // 定义模块选项
     this.addValues = function (values) {
 
         // 循环添加选项
@@ -56,11 +68,11 @@ function thePlayerList2D() {
         return 'Render';
     };
 
-    // 定义 [ theRenderPlayerList2D ] 用于存储需要渲染的文本;
+    // 定义 [ theRenderPlayerList2D ] 用于存储需要渲染的文本 Array<String>;
     var theRenderPlayerList2D;
     this.onUpdate = function () {
 
-        // 获取渲染文本的列表
+        // 获取渲染文本的列表 Array <String>
         theRenderPlayerList2D = getRenderList2D();
     };
     this.onRender2D = function () {
@@ -73,31 +85,31 @@ function thePlayerList2D() {
         }
     };
 
-    // 定义 [ getRenderList2D ] 用于获取渲染2D列表
+    // 定义 [ getRenderList2D ] 用于获取渲染2D列表 Array<String>
     function getRenderList2D() {
 
-        // 定义 [ renderList2D ] 用于存储渲染的文本
+        // 定义 [ renderList2D ] 用于存储渲染的文本 Array<String>
         var renderList2D = [];
 
-        // 定义 [ thisPlayerList ] 并存储获取到的玩家列表
-        var thisPlayerList = getPlayerList(settings.noNPC.get());
+        // 定义 [ thisPlayerList ] 并存储获取到的玩家列表 Array<net.minecraft.entity.player.EntityPlayer>
+        var thisPlayerList = getPlayerList(settings.invisible.get(), settings.noNPC.get(), settings.noBot.get());
 
         // 循环获取每个玩家对象
         for (var index = 0; index < thisPlayerList.length; index++) {
 
-            // 定义 [ player ] 获取并存储单独的玩家对象
+            // 定义 [ player ] 获取并存储单独的玩家对象 net.minecraft.entity.player.EntityPlayer
             var player = thisPlayerList[index];
 
-            // 数组中添加渲染玩家名称与声明的文本
+            // 数组中添加渲染玩家名称与声明的文本 String
             renderList2D.push(getPlayerRenderText(player));
 
-            // 如果自动渲染开启 当玩家所在 z < 700 时就渲染 或者 如果关闭自动渲染 任何时候都渲染工具
-            if (settings.renderTools.get() && mc.thePlayer.getPosition().getZ() < 700 || !settings.renderTools.get()) {
+            // 如果 显示工具列表2D开启 并且 在游戏中 并且 自动渲染开启 或者 如果自动渲染没开 并且 显示工具列表2D开启
+            if (settings.renderTools.get() && inGame() && settings.toolsList2D.get() || !settings.renderTools.get() && settings.toolsList2D.get()) {
 
                 // 尝试执行
                 try {
 
-                    // 获取工具渲染的列表
+                    // 获取工具渲染的列表 Array<String>
                     var toolsList2D = getToolsList2D(player);
 
                     // 循环获取 [ toolsList2D ] 中的每个文本
@@ -111,21 +123,55 @@ function thePlayerList2D() {
             }
         }
 
-        // 返回 renderList2D
+        // 返回 renderList2D Array<String>
         return renderList2D;
+    };
+
+    // 定义 [ getPlayerList ] (Boolean,Boolean) 用于获取全部玩家对象 Array<net.minecraft.entity.player.EntityPlayer>  
+    function getPlayerList(invisible, isAntiNPC, isAntiBot) {
+
+        // 创建 [ playerList ] 的数组用于存储符合条件的实体对象 Array<net.minecraft.entity.player.EntityPlayer>
+        var playerList = [];
+
+        // 循环遍历世界上每一个实体对象 Object
+        for (var index in mc.theWorld.loadedEntityList) {
+
+            // 创建 [ theEntity ] 用于存储每一个实体对象 net.minecraft.entity.Entity
+            var theEntity = mc.theWorld.loadedEntityList[index];
+
+            // 如果实体对象是实体玩家 
+            if (theEntity instanceof EntityPlayer) {
+
+                // 获取玩家的名称 java.lang.String
+                var name = theEntity.getName();
+
+                // 如果实体对象是隐身并且启用禁用隐身 或者 如果启用了禁用NPC并且名称为10并且不含有大写和下划线 或者 如果启用了防假人并且玩家名称红字
+                if (theEntity.isInvisible() && !invisible || isAntiNPC && name.length === 10 && !/[A-Z_]/.test(name) || isAntiBot && name.contains('§c')) {
+
+                    // 跳过循环
+                    continue;
+                };
+
+                // 添加到 [ playerList ] EntityPlayer
+                playerList.push(theEntity);
+            }
+        };
+
+        // 返回 [ playerList ] Array<net.minecraft.entity.player.EntityPlayer>
+        return playerList;
     }
 
 
-    // 定义 [ getPlayerRenderText ] 用于获取玩家的渲染文本
+    // 定义 [ getPlayerRenderText ] 用于获取玩家的渲染文本 String
     function getPlayerRenderText(thePlayer) {
 
-        // 获取当前血量
+        // 获取当前血量 Number
         var theEntityHealth = thePlayer.getHealth().toFixed(1);
 
-        // 获取最高血量
+        // 获取最高血量 float
         var theEntityMaxHealth = thePlayer.getMaxHealth();
 
-        // 获取名称
+        // 获取名称 java.lang.String
         var theEntityName = thePlayer.getName();
 
         // 如果实体是自身
@@ -134,22 +180,50 @@ function thePlayerList2D() {
             // 如果实体没受伤
             if (thePlayer.hurtTime == 0) {
 
-                // 深蓝字体名称 绿色字体血量
+                // 深蓝字体名称
                 return '§9' + theEntityName + '§f[§b ' + theEntityHealth + '§d/' + '§a' + theEntityMaxHealth + ' §f]';
-            }
+            };
 
-            // 血红字体名称 绿色字体血量
+            // 血红字体名称
             return '§4' + theEntityName + '§f[§b ' + theEntityHealth + '§d/' + '§a' + theEntityMaxHealth + ' §f]';
         }
 
         // 如果实体没受伤
         if (thePlayer.hurtTime == 0) {
 
-            // 亮绿字体名称 淡蓝
+            // 如果实体是隐身的
+            if (thePlayer.isInvisible()) {
+
+                // 灰色字体名称
+                return '§7' + theEntityName + '§f[§b ' + theEntityHealth + '§d/' + '§a' + theEntityMaxHealth + ' §f]';
+            };
+
+            // 亮绿字体名称
             return '§3' + theEntityName + '§f[§b ' + theEntityHealth + '§d/' + '§a' + theEntityMaxHealth + ' §f]';
-        }
+        };
+
         // 血红字体名称
         return '§4' + theEntityName + '§f[§b ' + theEntityHealth + '§d/' + '§a' + theEntityMaxHealth + ' §f]';
+    };
+
+    // 定义 [ inGame ] 用于判断是否在游戏中 Boolean
+    function inGame() {
+
+        // 尝试调用
+        try {
+
+            // 获取计分板名称 java.lang.String
+            var name = mc.theWorld.getScoreboard().getScores()[0].getObjective().getName();
+
+            // 判断name是否包含以下的字符串
+            return name === 'PreScoreboard' || name === 'health' || name === 'health_tab' || name === 'ZScoreboard';
+
+            // 报错就返回 false
+        } catch (error) {
+
+            // 如果没有计分板就不在游戏 因为游戏默认都有计分板
+            return false;
+        }
     }
 }
 
--- Tools.js
+++ Tools.js
@@ -2,7 +2,7 @@
 var scriptName = 'Tools';
 
 // 定义脚本版本
-var scriptVersion = '2.1.0';
+var scriptVersion = '2.1.1';
 
 // 定义脚本作者
 var scriptAuthor = ['ColdDragon'];
@@ -10,16 +10,13 @@ var scriptAuthor = ['ColdDragon'];
 // 定义 [ ItemSword ] 并引入mc的物品的剑类型 Class
 var ItemSword = Java.type('net.minecraft.item.ItemSword');
 
-// 引入实体玩家 Class
-var EntityPlayer = Java.type('net.minecraft.entity.player.EntityPlayer');
-
-// 定义 [ getToolsList2D ] 用于获取工具渲染的列表 Array
+// 定义 [ getToolsList2D ] 用于获取工具渲染的列表 Array<String>
 function getToolsList2D(player) {
 
-    // 定义 [ toolList2D ] 用于存储工具渲染的列表 Array
+    // 定义 [ toolList2D ] 用于存储工具渲染的列表 Array<String>
     var toolList2D = [];
 
-    // 定义 [ toolsList ] 获取并存储 Array
+    // 定义 [ toolsList ] 获取并存储 Array<any>
     var toolsList = getToolsList(player);
 
     // 循环 获取到的工具列表
@@ -57,39 +54,39 @@ function getRenderText(index, itemName, itemMaxDamage, lastDamage, toolStack, pl
     return '§4§l' + '■' + '§r' + itemName + '-' + '总耐久度:' + itemMaxDamage + '/' + '剩余耐久:' + lastDamage + toolStack;
 };
 
-// 定义 [ getToolsList ] 用于获取工具列表 Array
+// 定义 [ getToolsList ] 用于获取工具列表 Array<any>
 function getToolsList(player) {
 
-    // 定义 [ toolList ] 用于存储可用的工具对象 Array
+    // 定义 [ toolList ] 用于存储可用的工具对象 Array<any>
     var toolList = [];
 
-    // 如果玩家是自身 EntityPlayerSP == EntityPlayer
+    // 如果玩家是自身
     if (mc.thePlayer == player) {
 
-        // 返回一个获取自身的工具列表 Array
+        // 返回一个获取自身的工具列表 Array<SelfTool>
         return getSelfToolsList(); /* 这函数返回的本来就是个数组 */
     }
 
     // 尝试
     try {
 
-        // 获取其他玩家抓中的物品   注意 : 获取EntityOtherPlayerMP快捷栏0号将会获取到抓中的物品 /* ItemStack */
+        // 获取其他玩家抓中的物品   注意 : 获取EntityOtherPlayerMP快捷栏0号将会获取到抓中的物品 /* net.minecraft.item.ItemStack */
         var tool = player.inventory.getStackInSlot(0);
 
         // 如果物品非空
         if (tool) {
 
-            // 创建一个工具对象并存入 toolList
+            // 创建一个工具对象并存入 toolList 中
             toolList.push(new Tool(tool));
 
-            // 返回 toolList Array
+            // 返回 toolList Array<Tool>
             return toolList;
         }
 
         // 如果上述操作报错了
     } catch (error) {
 
-        // 返回一个空数组 Array
+        // 返回一个空数组 Array<any>
         return toolList;
     }
 }
@@ -121,16 +118,16 @@ function Tool(tool) {
     }
 }
 
-// 定义 [ getSelfToolsList ] 函数用于获取自己的工具列表 Array
+// 定义 [ getSelfToolsList ] 函数用于获取自己的工具列表 Array<SelfTool>
 function getSelfToolsList() {
 
-    // 定义 [ selfToolsList ] 用于存储合法的工具对象 Array
+    // 定义 [ selfToolsList ] 用于存储合法的工具对象 Array<SelfTool>
     var selfToolsList = [];
 
     // 循环4次 获取快捷栏的前4个物品
     for (var index = 0; index < 4; index++) {
 
-        // 获取物品 ItemStack
+        // 获取物品 net.minecraft.item.ItemStack
         var item = mc.thePlayer.inventory.getStackInSlot(index);
 
         // 如果物品为空
@@ -144,19 +141,19 @@ function getSelfToolsList() {
         // 获取物品最大耐久 integer
         var maxDamage = item.getMaxDamage();
 
-        // 如果最大耐久大于0 integer
+        // 如果最大耐久大于0
         if (maxDamage > 0) {
 
-            // 创建一个自己的工具对象并存储进 [ selfToolsList ] 中 push SelfTool Array
+            // 创建一个自己的工具对象并存储进 [ selfToolsList ] 中
             selfToolsList.push(new SelfTool(index, maxDamage, item));
         }
     }
 
-    // 返回 [ selfToolsList ] Array
+    // 返回 [ selfToolsList ] Array<SelfTool>
     return selfToolsList;
 }
 
-// 定义 自己的工具 对象的构造函数 传入3个参数 integer物品索引 integer物品最大耐久 ItemStack物品对象 构造函数
+// 定义 自己的工具 对象的构造函数 传入3个参数 (integer物品索引 integer物品最大耐久 net.minecraft.item.ItemStack物品对象) 构造函数
 function SelfTool(index, maxDamage, item) {
 
     // 存储索引 integer
@@ -165,7 +162,7 @@ function SelfTool(index, maxDamage, item) {
     // 存储最大耐久 integer
     this.maxDamage = maxDamage;
 
-    // 存储工具 ItemStack
+    // 存储工具 net.minecraft.item.ItemStack
     this.tool = item;
 
     // 获取并存储名称 String
@@ -233,52 +230,4 @@ function SelfTool(index, maxDamage, item) {
         // 返回判断是否在换弹 String
         return isLoad
     }
-}
-
-// 定义 [ getPlayerList ] 用于获取全部玩家对象 Array
-function getPlayerList(isAntiNPC) {
-
-    // 创建 [ playerList ] 的数组用于存储符合条件的实体对象 Array
-    var playerList = [];
-
-    // 循环遍历世界上每一个实体对象 Object
-    for (var index in mc.theWorld.loadedEntityList) {
-
-        // 创建 [ theEntity ] 用于存储每一个实体对象 Entity
-        var theEntity = mc.theWorld.loadedEntityList[index];
-
-        // 如果实体对象是实体玩家 
-        if (theEntity instanceof EntityPlayer) {
-
-            // 如果实体对象不是隐身
-            if (!theEntity.isInvisible()) {
-
-                // 如果防获取NPC开启
-                if (isAntiNPC) {
-
-                    // 获取当前实体 X integer
-                    var x = theEntity.getPosition().getX();
-
-                    // 如果实体的 x 大于 - 2538
-                    if (x > -2538) {
-
-                        // 获取 y integer
-                        var y = theEntity.getPosition().getY();
-
-                        // 如果 y 是 54 或者 y 是 55
-                        if (y === 54 || y === 55) {
-
-                            // 跳过循环
-                            continue;
-                        }
-                    }
-                }
-
-                // 添加到 [ playerList ] EntityPlayer
-                playerList.push(theEntity);
-            }
-        }
-    }
-    // 返回 [ playerList ] Array
-    return playerList;
 }
