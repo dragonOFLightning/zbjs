2b1e82105c160814296801948a622495bd07bc0e
3dragons <3dragons@cd.com>
2023-08-30 23:15:48 +0800

    ZombiesList3D.js add dangerRender

--- ZombiesList3D.js
+++ ZombiesList3D.js
@@ -4,79 +4,86 @@
 var scriptName = 'ZombiesList3D';
 
 // 定义脚本版本
-var scriptVersion = '0.1.5';
+var scriptVersion = '0.2.0';
 
 // 定义脚本作者
 var scriptAuthor = ['ColdDragon'];
 
-// 引入渲染工具 [ RenderUtils ]
+// 引入工具函数 @JavaScript
+script.import('Tools.js');
+
+// 引入渲染工具 [ RenderUtils ] @Class
 var RenderUtils = Java.type('net.ccbluex.liquidbounce.utils.render.RenderUtils');
 
-// 引入颜色对象 [ Color ]
+// 引入颜色对象 [ Color ] @Class
 var Color = Java.type('java.awt.Color');
 
-// 引入容器对象 [ AxisAlignedBB ]
+// 引入容器对象 [ AxisAlignedBB ] @Class
 var AxisAlignedBB = Java.type('net.minecraft.util.AxisAlignedBB');
-
-// 定义 [ ZombiesEntity ] 用于存储所需的实体类型
-var ZombiesEntity = {
-
-    // 引入实体类型 [ EntityZombie ] - 僵尸
-    EntityZombie: Java.type('net.minecraft.entity.monster.EntityZombie'),
-
-    // 引入实体类型 [ EntityPigZombie ] - 猪僵尸
-    EntityPigZombie: Java.type('net.minecraft.entity.monster.EntityPigZombie'),
-
-    // 引入实体类型 [ EntityMagmaCube ] - 岩浆怪
-    EntityMagmaCube: Java.type('net.minecraft.entity.monster.EntityMagmaCube'),
-
-    // 引入实体类型 [ EntitySlime ] - 史莱姆
-    EntitySlime: Java.type('net.minecraft.entity.monster.EntitySlime'),
-
-    // 引入实体类型 [ EntityIronGolem ] - 铁傀儡
-    EntityGolem: Java.type('net.minecraft.entity.monster.EntityIronGolem'),
-
-    // 引入实体类型 [ EntityWolf ] - 狼
-    EntityWolf: Java.type('net.minecraft.entity.passive.EntityWolf'),
-
-    // 引入实体类型 [ EntityGiantZombie ] - 巨人僵尸
-    EntityGiantZombie: Java.type('net.minecraft.entity.monster.EntityGiantZombie'),
-
-    // 引入实体类型 [ EntityGhast ] - 恶魂
-    EntityGhast: Java.type('net.minecraft.entity.monster.EntityGhast'),
-
-    // 引入实体类型 [ EntityBlaze ] - 烈焰
-    EntityBlaze: Java.type('net.minecraft.entity.monster.EntityBlaze'),
-
-    // 引入实体类型 [ EntitySkeleton ] - 骷髅
-    EntitySkeleton: Java.type('net.minecraft.entity.monster.EntitySkeleton'),
-
-    // 引入实体类型 [ EntityCreeper ] - 爬行者
-    EntityCreeper: Java.type('net.minecraft.entity.monster.EntityCreeper'),
-
-    // 引入实体类型 [ EntityWitch ] - 女巫
-    EntityWitch: Java.type('net.minecraft.entity.monster.EntityWitch'),
-
-    // 引入实体类型 [ EntityCaveSpider ] - 洞穴蜘蛛
-    EntityCaveSpider: Java.type('net.minecraft.entity.monster.EntityCaveSpider'),
-
-    // 引入实体类型 [ EntityEndermite ] - 末影螨
-    EntityEndermite: Java.type('net.minecraft.entity.monster.EntityEndermite'),
-
-    // 引入实体类型 [ EntityWither ] - 凋灵
-    EntityWither: Java.type('net.minecraft.entity.boss.EntityWither'),
-}
 
 // 定义模块的构造函数 [ theZombiesList3D ]
 function theZombiesList3D() {
 
+    // 定义 [ ZombiesEntity ] 用于存储所需的实体类型
+    var ZombiesEntity = {
+
+        // 引入实体类型 [ EntityZombie ] - 僵尸
+        EntityZombie: Java.type('net.minecraft.entity.monster.EntityZombie'),
+
+        // 引入实体类型 [ EntityPigZombie ] - 猪僵尸
+        EntityPigZombie: Java.type('net.minecraft.entity.monster.EntityPigZombie'),
+
+        // 引入实体类型 [ EntityMagmaCube ] - 岩浆怪
+        EntityMagmaCube: Java.type('net.minecraft.entity.monster.EntityMagmaCube'),
+
+        // 引入实体类型 [ EntitySlime ] - 史莱姆
+        EntitySlime: Java.type('net.minecraft.entity.monster.EntitySlime'),
+
+        // 引入实体类型 [ EntityIronGolem ] - 铁傀儡
+        EntityGolem: Java.type('net.minecraft.entity.monster.EntityIronGolem'),
+
+        // 引入实体类型 [ EntityWolf ] - 狼
+        EntityWolf: Java.type('net.minecraft.entity.passive.EntityWolf'),
+
+        // 引入实体类型 [ EntityGiantZombie ] - 巨人僵尸
+        EntityGiantZombie: Java.type('net.minecraft.entity.monster.EntityGiantZombie'),
+
+        // 引入实体类型 [ EntityGhast ] - 恶魂
+        EntityGhast: Java.type('net.minecraft.entity.monster.EntityGhast'),
+
+        // 引入实体类型 [ EntityBlaze ] - 烈焰
+        EntityBlaze: Java.type('net.minecraft.entity.monster.EntityBlaze'),
+
+        // 引入实体类型 [ EntitySkeleton ] - 骷髅
+        EntitySkeleton: Java.type('net.minecraft.entity.monster.EntitySkeleton'),
+
+        // 引入实体类型 [ EntityCreeper ] - 爬行者
+        EntityCreeper: Java.type('net.minecraft.entity.monster.EntityCreeper'),
+
+        // 引入实体类型 [ EntityWitch ] - 女巫
+        EntityWitch: Java.type('net.minecraft.entity.monster.EntityWitch'),
+
+        // 引入实体类型 [ EntityCaveSpider ] - 洞穴蜘蛛
+        EntityCaveSpider: Java.type('net.minecraft.entity.monster.EntityCaveSpider'),
+
+        // 引入实体类型 [ EntityEndermite ] - 末影螨
+        EntityEndermite: Java.type('net.minecraft.entity.monster.EntityEndermite'),
+
+        // 引入实体类型 [ EntityWither ] - 凋灵
+        EntityWither: Java.type('net.minecraft.entity.boss.EntityWither'),
+    };
+
+    // 定义 [ theRenderEntityList3D ] 用于存储需要渲染的实体对象 @Array<ZombiesEntity>
+    var theRenderEntityList3D = [];
+
+    // 定义 [round] 用于存储当前场景 @string
+    var ground = 'Lobby';
+
+    // 定义 [tick] 用于计数 @integer
+    var tick = -1;
+
     // 定义 [ setting ] 对象 用于提供设置的选项
     var setting = {
-
-        // 浮点数选项 [ 名称 默认值 最小值 最大值 ]
-        float: function (name, def, min, max) {
-            return value.createFloat(name, def, min, max);
-        },
 
         // 整数选项 [ 名称 默认值 最小值 最大值 ]
         integer: function (name, def, min, max) {
@@ -87,22 +94,25 @@ function theZombiesList3D() {
         boolean: function (name, def) {
             return value.createBoolean(name, def);
         },
-
-        // 列表选项 [ 名称 数组 默认值 ]
-        list: function (name, values, def) {
-            return value.createList(name, values, def);
-        }
     }
 
     // 定义 [ settings ] 用于存储选项
     var settings = {
-        // 避免获取到凋零血量标题
+
+        // 避免获取到标题凋灵
         theAntiTitle: setting.boolean('AntiTitle', true),
-        // 设置 三原色 和 透明度
+
+        // 设置 常规僵尸 三原色 和 透明度
         theRed: setting.integer('Red', 30, 0, 255),
         theGreen: setting.integer('Green', 170, 0, 255),
         theBlue: setting.integer('Blue', 255, 0, 255),
         theAlpha: setting.integer('Alpha', 50, 0, 255),
+
+        // 设置 重大威胁的僵尸 三原色 和 透明度
+        theDangerRed: setting.integer('DangerRed', 255, 0, 255),
+        theDangerGreen: setting.integer('DangerGreen', 0, 0, 255),
+        theDangerBlue: setting.integer('DangerBlue', 0, 0, 255),
+        theDangerAlpha: setting.integer('DangerAlpha', 50, 0, 255),
     };
 
     // 定义模块选项
@@ -131,14 +141,74 @@ function theZombiesList3D() {
         return 'Render';
     };
 
-    // 定义 [ theRenderEntityList3D ] 用于存储需要渲染的实体对象
-    var theRenderEntityList3D = [];
+    // 定义 [atLobby] 用于判断是否在大厅 @boolean
+    var atLobby = true;
 
     // 定义模块更新
     this.onUpdate = function () {
 
         // 把获取到的实体对象存储到 [ theRenderEntityList3D ] 中
         theRenderEntityList3D = getZombiesList3D();
+
+        // tick为 如果tick < 0 则 tick 否则 tick 自减
+        tick = tick < 0 ? tick : tick - 1;
+
+        // 定义并获取z轴 @int
+        var z = mc.thePlayer.getPosition().getZ();
+
+        atLobby = !imGame();
+
+        // 通过z判断当前场景
+        ground = atLobby ? 'Lobby' : tick > 0 ? z > 10 ? 'DeadEnd' : z > 7 ? 'BadBlood' : 'AlienArcadium' : ground;
+    };
+
+    // 定义 [round] 用于存储当前的round值 @integer
+    var round = 1;
+
+    // 引入 服务器标题数据包 @Class
+    var S45PacketTitle = Java.type('net.minecraft.network.play.server.S45PacketTitle');
+
+    // 引入 服务器聊天数据包 @Class
+    var S02PacketChat = Java.type('net.minecraft.network.play.server.S02PacketChat');
+
+    this.onPacket = function (event) {
+
+        // 获取数据包 @any
+        var thePacket = event.getPacket();
+
+        // 如果数据包类型是 [S45PacketTitle]
+        if (thePacket instanceof S45PacketTitle) {
+
+            // 定义并获取标题文本 @java.lang.String
+            var titleText = thePacket.getMessage().getUnformattedText();
+
+            // 如果标题文本中包含 ['Round']
+            if (titleText.contains('Round')) {
+
+                // 定义并获取round后面的索引 round的长度是5 而 round和数字之间有1个空格 @integer
+                var index = titleText.indexOf('Round') + 6;
+
+                // 截取字符串round @string
+                var string = titleText.substring(index, titleText.length);
+
+                // 如果为1 表示游戏刚开始 赋值为10[数值越大精度越高也越易被干扰]
+                tick = string == 1 ? 10 : tick;
+
+                // 强制integer存储进round中
+                round = parseInt(string);
+            }
+        }
+        // 否则 如果数据包类型是 [S02PacketChat]
+        else if (thePacket instanceof S02PacketChat) {
+
+            // 定义并获取聊天文本 @java.lang.String
+            var chatText = thePacket.getChatComponent().getUnformattedText();
+
+            if (chatText.contains('Sending you to mini') && chatText.contains('!')) {
+                // 赋值 tick 为30
+                tick = 30;
+            }
+        }
     };
 
     // 定义模块渲染3D
@@ -147,7 +217,7 @@ function theZombiesList3D() {
         // 遍历 [ theZombieList3D ] 中的每一个 实体对象
         for (var index = 0; index < theRenderEntityList3D.length; index++) {
 
-            // 获取实体对象 @any
+            // 获取实体对象 @theZombiesList3D
             var theEntity = theRenderEntityList3D[index];
 
             // 获取实体的生命值 @float
@@ -184,17 +254,20 @@ function theZombiesList3D() {
             // 获取实体的名称 @java.lang.String
             var theEntityName = theEntity.getName();
 
+            // 获取实体的总血量 @float
+            var theEntityMaxHealth = theEntity.getMaxHealth();
+
             // 如果实体没有受伤
             if (theEntity.hurtTime == 0) {
 
                 // 存储文本 白色的名称
-                theRenderText = '§f' + theEntityName + '§2[' + '§b' + theEntityHealth + '§2]';
+                theRenderText = '§f' + theEntityName + '§2[' + '§b' + theEntityHealth + '/' + theEntityMaxHealth + '§2]';
 
                 // 否则
             } else {
 
                 // 存储文本 血红的名称
-                theRenderText = '§4' + theEntityName + '§2[' + '§b' + theEntityHealth + '§2]';
+                theRenderText = '§4' + theEntityName + '§2[' + '§b' + theEntityHealth + '/' + theEntityMaxHealth + '§2]';
             }
 
             // 渲染文本
@@ -219,15 +292,18 @@ function theZombiesList3D() {
             // 创建颜色对象 存入颜色
             var theColor = new Color(settings.theRed.get(), settings.theGreen.get(), settings.theBlue.get(), settings.theAlpha.get());
 
+            // 创建颜色对象 存入颜色
+            var theDangerColor = new Color(settings.theDangerRed.get(), settings.theDangerGreen.get(), settings.theDangerBlue.get(), settings.theDangerAlpha.get());
+
             // 渲染容器
-            RenderUtils.drawAxisAlignedBB(theBox, theColor);
+            RenderUtils.drawAxisAlignedBB(theBox, isDangerZombie(theEntity) ? theDangerColor : theColor);
         }
     };
 
-    // 定义 [ getZombiesList3D ] 用于获取符合条件的实体对象
+    // 定义 [ getZombiesList3D ] 用于获取符合条件的实体对象 @Array<ZombiesEntity>
     function getZombiesList3D() {
 
-        // 定义 [ theZombieList ] 用于存储符合条件的实体对象
+        // 定义 [ theZombieList ] 用于存储符合条件的实体对象 @Array<ZombiesEntity>
         var theZombieList = [];
 
         // 获取世界上每一个实体
@@ -246,7 +322,7 @@ function theZombiesList3D() {
                     if (settings.theAntiTitle.get()) {
 
                         // 如果实体类型是凋零并且血量为300
-                        if (theEntity instanceof ZombiesEntity.EntityWither && theEntity.getHealth() == 300) {
+                        if (theEntity instanceof ZombiesEntity.EntityWither && theEntity.getName().length > 20) {
 
                             // 跳过循环
                             continue;
@@ -257,9 +333,29 @@ function theZombiesList3D() {
                     theZombieList.push(theEntity);
                 }
             }
-        }
+        };
         // 返回 [ theZombieList ]
         return theZombieList;
+    };
+
+    // 定义 [ isDangerZombie ] 判断此僵尸是否是一个危险等级的僵尸 @boolean
+    function isDangerZombie(theEntity) {
+
+        // 巨人僵尸直接 true
+        if (theEntity instanceof ZombiesEntity.EntityGiantZombie) {
+            return true;
+        }
+        // 否则 如果是僵尸
+        else if (theEntity instanceof ZombiesEntity.EntityZombie) {
+
+            // 满足以下条件的僵尸就是危险等级的僵尸
+            if (round > 39 && round < 60 && theEntity.getMaxHealth() > 249 ||
+                round === 60 && theEntity.getHeldItem().getItem() instanceof ItemSword ||
+                round > 63 && theEntity.getMaxHealth() > 599 ||
+                ground !== 'AlienArcadium' && theEntity.getMaxHealth() > 100) {
+                return true;
+            }
+        }
     };
 }
 
