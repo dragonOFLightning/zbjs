a656925c82f9d2d9b3613e1300479adcd9f04278
3dragons <3dragons@dragons.com>
2024-03-24 21:30:27 +0800

    add README

--- test.js
+++ test.js
@@ -1,1 +1,52 @@
+/*
+Creeperssss_ opened Ferris Wheel!
+Snack_Powder opened Roller Coaster!
+Creeperssss_ opened Bumper Cars!
++255 coins! (Double Coins + EXP, Grapex's Network Booster)
+The game starts in 1 second!
 
+Creeperssss_ opened Graveyard!
+CheemsSheep opened Mansion!
+CheemsSheep opened Library!
+CheemsSheep opened Dungeon!
+CheemsSheep opened Balcony!
+     has spawned in the Great Hall!
+Creeperssss_ opened Crypts!
+
+YanziCN_2023 opened Office!
+DanDaPenguin232 opened Gallery!
+YanziCN_2023 opened Hotel!
+YanziCN_2023 opened Apartments!
+YanziCN_2023 opened Power Station!
+YanziCN_2023 opened Roof Top!
+This game has been recorded. Click here to watch the Replay!
+*/
+
+// Sending you to mini69CU!
+// Sending you to mini195S!
+// Sending you to mini158C!
+
+//    Progress through the map to buy better weapons
+
+// 13 68 19 de
+// 0 72 5 aa
+// 24 68 9 bb
+
+// mob.irongolem.hit        pistol
+// random.successful_hit    命中
+// random.explode           shot gun
+// mob.horse.gallop         reload
+// random.door_open         open door
+// mob.zombie.metal         fixed windows
+// mob.zombie.wood          fixing windows
+// fireworks.blast_far      sniper
+// random.bow               BlowDart || 弓射击
+// ambient.weather.thunder  ElderGun || 打雷
+// fire.ignite              ZombieZapper
+// dig.stone                GoldDigger
+// fireworks.largeBlast     Rifle || RainbowRifle
+
+// +9硬币！ (Double Coins + EXP, _josep的Network Booster)
+// +9 coins! (Double Coins + EXP, _josep's Network Booster)
+
+// Lovests was knocked down by Zombie in Alley! You have 25s to get revived! 
