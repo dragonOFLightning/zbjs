b250036880c167b4c46cd1ab75994e663db3d7c9
3dragons <3dragons@dragons.com>
2024-04-01 17:10:16 +0800

    Refactoring TickChange

--- TickChange.js
+++ TickChange.js
@@ -4,21 +4,18 @@
  * 同步切枪 TickChange 与最近的玩家同步切换快捷栏
  */
 
-// 定义脚本名称
+/**定义脚本名称 */
 var scriptName = 'TickChange'
 
-// 定义脚本版本
-var scriptVersion = '1.1.0'
+/**定义脚本版本 */
+var scriptVersion = '1.2.0'
 
-// 定义脚本作者
+/**定义脚本作者 */
 var scriptAuthor = ['ColdDragon']
 
-/**实体玩家类型 */
-var EntityPlayer = Java.type('net.minecraft.entity.player.EntityPlayer')
-
-/**定义 [ setting ] 对象 用于提供选项 */
+/**提供选项 */
 var setting = {
-    
+
     /**
      * @function boolean 布尔值选项
      * @param {string} name 选项名称
@@ -26,7 +23,7 @@ var setting = {
      * @returns {object} 选项
      */
     boolean: function (name, def) {
-        return value.createBoolean(name, def);
+        return value.createBoolean(name, def)
     },
 
     /**
@@ -36,86 +33,77 @@ var setting = {
      * @returns {object} 选项
      */
     text: function (name, def) {
-        return value.createText(name, def);
+        return value.createText(name, def)
     },
 }
 
+/**设置选项 */
 var settings = {
-
     /**使用白玩家 */
-    useWhitePlayer: setting.boolean('UseWhitePlayer', false),
+    whitePlayerMode: setting.boolean('WhitePlayerMode', false),
 
     /**白玩家设置 */
     whitePlayer: setting.text('WhitePlayer', '3claws_dragons'),
 }
 
-// 定义模块的构造函数
-function theTickChange() {
+/**@function TickChange_onUpdate 每tick调用 */
+function TickChange_onUpdate() {
+    /**@type {net.minecraft.entity.player.EntityPlayer} 同步的玩家*/
+    var player = settings.whitePlayerMode.get() ? getPlayer(settings.whitePlayer.get()) : getClosePlayer()
 
-    // 定义模块名称
-    this.getName = function () {
-        return 'TickChange'
+    // 如果没获取到 就结束
+    if (!player) {
+        return
     }
 
-    // 定义模块描述
-    this.getDescription = function () {
-        return 'ColdDragon'
+    /**@type {java.lang.String} 同步的玩家当前持有的物品*/
+    var item = player.getHeldItem()
+
+    // 如果没有持有物品
+    if (!item) {
+        var emptySlot = getEmptySlot()
+        mc.thePlayer.inventory.currentItem = emptySlot === null ? mc.thePlayer.inventory.currentItem : emptySlot
+        return
     }
 
-    // 定义模块归类
-    this.getCategory = function () {
-        return 'Fun'
-    }
+    /**@type {java.lang.String} 同步的玩家当前持有的物品名称*/
+    var itemName = item.getUnlocalizedName()
 
-    // 模块更新时调用
-    this.onUpdate = function () {
-        /**@type {net.minecraft.entity.player.EntityPlayer} 同步的玩家*/
-        var player = settings.useWhitePlayer.get() ? getPlayer(settings.whitePlayer.get()) : getClosePlayer()
+    // 反向循环获取快捷栏 反向循环作用是给卡假枪的用的 正常情况和正向循环无差别
+    for (var slot = 2 << 2; slot > -1; slot--) {
 
-        // 如果没获取到 就结束
-        if (!player) {
-            return
+        /**@type {net.minecraft.item.ItemStack} 当前槽位的物品*/
+        var selfItem = mc.thePlayer.inventory.getStackInSlot(slot)
+
+        // 如果当前槽位没有物品 则跳过
+        if (!selfItem) {
+            continue
         }
 
-        /**@type {java.lang.String} 同步的玩家当前持有的物品*/
-        var item = player.getHeldItem()
+        /**@type {java.lang.String} 当前槽位物品名称*/
+        var selfItemName = selfItem.getUnlocalizedName()
 
-        // 如果没有持有物品
-        if (!item) {
-            var emptySlot = getEmptySlot()
-            mc.thePlayer.inventory.currentItem = emptySlot === null ? mc.thePlayer.inventory.currentItem : emptySlot
-            return
-        }
+        // 如果当前槽位物品名称 和 同步的玩家持有的物品相同
+        if (selfItemName === itemName) {
 
-        /**@type {java.lang.String} 同步的玩家当前持有的物品名称*/
-        var itemName = item.getUnlocalizedName()
-
-        // 反向循环获取快捷栏 反向循环作用是给卡假枪的用的 正常情况和正向循环无差别
-        for (var slot = 2 << 2; slot > -1; slot--) {
-
-            /**@type {net.minecraft.item.ItemStack} 当前槽位的物品*/
-            var selfItem = mc.thePlayer.inventory.getStackInSlot(slot)
-
-            // 如果当前槽位没有物品 则跳过
-            if (!selfItem) {
-                continue
-            }
-
-            /**@type {java.lang.String} 当前槽位物品名称*/
-            var selfItemName = selfItem.getUnlocalizedName()
-
-            // 如果当前槽位物品名称 和 同步的玩家持有的物品相同
-            if (selfItemName === itemName) {
-
-                // 切换快捷栏
-                mc.thePlayer.inventory.currentItem = slot
-            }
+            // 切换快捷栏
+            mc.thePlayer.inventory.currentItem = slot
         }
     }
 }
 
+/**@class TickChange 模块*/
+function TickChange() { }
+/**@override 模块名称*/
+TickChange.prototype.getName = function () { return 'TickChange' }
+/**@override 模块描述*/
+TickChange.prototype.getDescription = function () { return 'ColdDragon' }
+/**@override 模块类型*/
+TickChange.prototype.getCategory = function () { return 'Fun' }
+/**@override */
+TickChange.prototype.onUpdate = TickChange_onUpdate
 /**@override 覆写添加值函数 用于给模块添加选项*/
-theTickChange.prototype.addValues = function (values) {
+TickChange.prototype.addValues = function (values) {
     for (var key in settings) {
         values.add(settings[key])
     }
@@ -137,6 +125,9 @@ function getEmptySlot() {
     }
     return null;
 }
+
+/**实体玩家类型 */
+var EntityPlayer = Java.type('net.minecraft.entity.player.EntityPlayer')
 
 /**
  * @function getPlayer 获取玩家实体
@@ -202,29 +193,26 @@ function getClosePlayer() {
     return null
 }
 
-// 脚本启用时调用
+/**脚本启用时调用 */
 function onLoad() {
 
-    // 输出一句话表示正常
-    chat.print('§9' + 'TickChange' + ' §2- §4Load')
+    // 文字反馈模块已加载
+    chat.print('§9TickChange §2- §4Load')
 }
 
-// 创建模块的实例 [ dragonTickChange ]
-var dragonTickChange = new theTickChange()
+/**@type {object} 定义脚本模块*/
+var scriptModule
 
-// 定义 [ dragonTickChange ] 用于存储注册信息
-var dragonTickChangeClient
-
-// 脚本运行时调用
+/**脚本运行时调用 */
 function onEnable() {
 
-    // 注册
-    dragonTickChangeClient = moduleManager.registerModule(dragonTickChange)
+    // 注册模块
+    scriptModule = moduleManager.registerModule(new TickChange())
 }
 
-// 脚本禁用时调用
+/**脚本禁用时调用 */
 function onDisable() {
 
-    // 注销
-    moduleManager.unregisterModule(dragonTickChangeClient)
+    // 注销模块
+    moduleManager.unregisterModule(scriptModule)
 }
