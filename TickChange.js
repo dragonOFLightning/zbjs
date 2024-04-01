dfd55fcc6c44db4ff15c0e5b437614b52e786d85
3dragons <3dragons@dragons.com>
2024-04-01 16:39:07 +0800

    TickChange.js update : add settings useWhitePlayer & whitePlayer

--- TickChange.js
+++ TickChange.js
@@ -16,6 +16,39 @@ var scriptAuthor = ['ColdDragon']
 /**实体玩家类型 */
 var EntityPlayer = Java.type('net.minecraft.entity.player.EntityPlayer')
 
+/**定义 [ setting ] 对象 用于提供选项 */
+var setting = {
+    
+    /**
+     * @function boolean 布尔值选项
+     * @param {string} name 选项名称
+     * @param {boolean} def 选项默认值
+     * @returns {object} 选项
+     */
+    boolean: function (name, def) {
+        return value.createBoolean(name, def);
+    },
+
+    /**
+     * @function text 文本选项
+     * @param {string} name 选项名称 
+     * @param {string} def 选项默认值
+     * @returns {object} 选项
+     */
+    text: function (name, def) {
+        return value.createText(name, def);
+    },
+}
+
+var settings = {
+
+    /**使用白玩家 */
+    useWhitePlayer: setting.boolean('UseWhitePlayer', false),
+
+    /**白玩家设置 */
+    whitePlayer: setting.text('WhitePlayer', '3claws_dragons'),
+}
+
 // 定义模块的构造函数
 function theTickChange() {
 
@@ -36,15 +69,15 @@ function theTickChange() {
 
     // 模块更新时调用
     this.onUpdate = function () {
-        /**@type {net.minecraft.entity.player.EntityPlayer} 最近的玩家*/
-        var player = getClosePlayer()
+        /**@type {net.minecraft.entity.player.EntityPlayer} 同步的玩家*/
+        var player = settings.useWhitePlayer.get() ? getPlayer(settings.whitePlayer.get()) : getClosePlayer()
 
         // 如果没获取到 就结束
         if (!player) {
             return
         }
 
-        /**@type {java.lang.String} 最近的玩家当前持有的物品*/
+        /**@type {java.lang.String} 同步的玩家当前持有的物品*/
         var item = player.getHeldItem()
 
         // 如果没有持有物品
@@ -54,7 +87,7 @@ function theTickChange() {
             return
         }
 
-        /**@type {java.lang.String} 最近的玩家当前持有的物品名称*/
+        /**@type {java.lang.String} 同步的玩家当前持有的物品名称*/
         var itemName = item.getUnlocalizedName()
 
         // 反向循环获取快捷栏 反向循环作用是给卡假枪的用的 正常情况和正向循环无差别
@@ -71,13 +104,20 @@ function theTickChange() {
             /**@type {java.lang.String} 当前槽位物品名称*/
             var selfItemName = selfItem.getUnlocalizedName()
 
-            // 如果当前槽位物品名称 和 最近的玩家持有的物品相同
+            // 如果当前槽位物品名称 和 同步的玩家持有的物品相同
             if (selfItemName === itemName) {
 
                 // 切换快捷栏
                 mc.thePlayer.inventory.currentItem = slot
             }
         }
+    }
+}
+
+/**@override 覆写添加值函数 用于给模块添加选项*/
+theTickChange.prototype.addValues = function (values) {
+    for (var key in settings) {
+        values.add(settings[key])
     }
 }
 
@@ -99,7 +139,39 @@ function getEmptySlot() {
 }
 
 /**
- * @function getClosePlayer 获取最近的实体  
+ * @function getPlayer 获取玩家实体
+ * 
+ * @param {string|java.lang.String} name 
+ */
+function getPlayer(name) {
+
+    /**世界实体 */
+    var worldEntity = mc.theWorld.loadedEntityList
+
+    // 遍历世界实体
+    for (var key in worldEntity) {
+
+        /**@type {net.minecraft.entity.Entity} 实体*/
+        var entity = worldEntity[key]
+
+        /**@type {boolean} 是否是玩家*/
+        var isPlayer = entity instanceof EntityPlayer
+
+        // 跳过 非玩家 或者 死实体
+        if (!isPlayer || entity.isDead) {
+            continue
+        }
+
+        // 如果名称相同 则返回实体
+        if (name === entity.getName()) {
+            return entity
+        }
+    }
+    return null
+}
+
+/**
+ * @function getClosePlayer 获取最近的玩家实体  
  */
 function getClosePlayer() {
 
